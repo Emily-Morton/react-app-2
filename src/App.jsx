4ecf39f3e9d9e78e1bd028ff2f5fb824f4c44118
Emily Morton <emilyrosemorton@gmail.com>
2025-06-05 19:25:02 +0100

    adding pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-// import { useState, useEffect } from 'react'
-import './App.css'
+// import { useState, useEffect } from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
+import "./App.css";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router";
 
 function App() {
-
-  return (<>
-  <h1>Hello World</h1>
-  <p>paragraph</p>
-  </>)
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Router>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
